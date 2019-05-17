f812785f71485c048995804bb93acf213c4f5df4
Tal Koren <talkor@users.noreply.github.com>
2019-05-17 11:43:17 +0300

    Basic wireframe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Example from './example/example.jsx';
-
-const App = () => (
-    <div>
-        <Example />       
-    </div>
-);
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App.jsx';
+ReactDOM.render(<App />, document.getElementById('root'));

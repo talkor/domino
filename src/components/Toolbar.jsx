74b37e567225b575bf58972a2ef2bf88c6a64651
Tal Koren <talkor@users.noreply.github.com>
2019-05-30 15:06:02 +0300

    Add toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -8,7 +8,15 @@ class Toolbar extends React.Component {
   }
 
   render() {
-    return <div className="timer" />;
+    return (
+      <div className="toolbar">
+        <span className="timer">00:00</span>
+        <span>{this.props.numTurns}</span>
+        <button className="game-button new">New Game</button>
+        <button className="game-button prev">Prev</button>
+        <button className="game-button next">Next</button>
+      </div>
+    );
   }
 }
 

ab6769fbf22e7ccaf0099686f25aa949504c09af
Tal Koren <talkor@users.noreply.github.com>
2019-06-03 23:51:40 +0300

    Minor

--- src/components/PlayerStack.jsx
+++ src/components/PlayerStack.jsx
@@ -6,27 +6,23 @@ const PlayerStack = props => {
   const handleTileClick = event => {
     event.preventDefault();
     const selectedTile = event.currentTarget.dataset.tile;
-    props.setSelectedTile(selectedTile);
+    if (!props.isGameOver) {
+      props.setSelectedTile(selectedTile);
+    }
   };
 
   return (
     <div className="player-stack">
-      {props.isGameOver ? (
-        ''
-      ) : (
-        <React.Fragment>
-          {props.playerTiles.map((tile, key) => {
-            return (
-              <Tile
-                tile={tile}
-                key={key}
-                selected={props.selectedTile == tile}
-                onTileClick={handleTileClick.bind(this)}
-              />
-            );
-          })}
-        </React.Fragment>
-      )}
+      {props.playerTiles.map((tile, key) => {
+        return (
+          <Tile
+            tile={tile}
+            key={key}
+            selected={props.selectedTile == tile}
+            onTileClick={handleTileClick.bind(this)}
+          />
+        );
+      })}
     </div>
   );
 };
